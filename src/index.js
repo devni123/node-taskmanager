6411fd9381e1e81063515a3ecc464b4bd54b3257
Devni Heraliyawala <devni.heraliyawala@synapsys.lk>
2020-04-04 16:21:37 +0530

    RESTful APIs for create and read data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,83 @@
 const express = require('express')
+require('./db/mongoose')
+
+const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.post('/users', (req, res) => {
+
+    const user = new User(req.body)
+
+    user.save().then(() => {
+        res.status(201).send(user)
+
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+app.get('/users', (req, res) => {
+
+    User.find({}).then((users) => {
+        return res.send(users)
+    }).catch((e) => {
+        return res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        return res.send(user)
+    }).catch((e) => {
+        return res.status(500).send(e)
+    })
+})
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+
+    task.save().then(() => {
+        res.status(201).send(task)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+
+app.get('/tasks', (req, res) => {
+
+    Task.find({}).then((tasks) => {
+        return res.send(tasks)
+    }).catch((e) => {
+        return res.status(500).send()
+    })
+
+})
+
+
+app.get('/tasks/:id', (req, res) => {
+
+    const _id = req.params.id
+
+
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send()
+        }
+        return res.send(task)
+    }).catch((e) => {
+        return res.status(500).send()
+    })
+})
 
 app.listen(port, () => {
     console.log('Express server is up on port ' + port)
