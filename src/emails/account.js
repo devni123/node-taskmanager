20c36bb8058b3e86e05a43afae30ec50f5c6648e
Devni Heraliyawala <devni.heraliyawala@synapsys.lk>
2020-04-13 13:32:10 +0530

    re-enabled email sending feature with sendgrid

--- src/emails/account.js
+++ src/emails/account.js
@@ -13,21 +13,21 @@ const msg = {
 
 
 const sendWelcomeEmail = (email, name) => {
-    // sgMail.send({
-    //     to: email,
-    //     from: 'devniheraliyawala314@gmail.com',
-    //     subject: 'Thanks for joining in!',
-    //     text: `Welcome to the app ${name}. Let me know how you get along with the app.`
-    // })
+    sgMail.send({
+        to: email,
+        from: 'devniheraliyawala314@gmail.com',
+        subject: 'Thanks for joining in!',
+        text: `Welcome to the app ${name}. Let me know how you get along with the app.`
+    })
 }
 
 const sendCancelationEmail = (email, name) => {
-    // sgMail.send({
-    //     to: email,
-    //     from: 'devniheraliyawala314@gmail.com',
-    //     subject: 'Sorry to see you go!',
-    //     text: `Good bye ${name}. Is there anything we could have done to kept you onboard `
-    // })
+    sgMail.send({
+        to: email,
+        from: 'devniheraliyawala314@gmail.com',
+        subject: 'Sorry to see you go!',
+        text: `Good bye ${name}. Is there anything we could have done to kept you onboard `
+    })
 }
 
 module.exports = {
